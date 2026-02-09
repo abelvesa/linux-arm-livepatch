2c95c01ef6d2bd9d2bb6100875a086004b471ca1
Abel Vesa <abel.vesa@oss.qualcomm.com>
2026-02-09 11:33:41 +0200

    fixup! dt-bindings: clock: qcom: Document the Eliza TCSR Clock Controller

--- include/dt-bindings/clock/qcom,eliza-tcsr.h
+++ include/dt-bindings/clock/qcom,eliza-tcsr.h
@@ -7,9 +7,11 @@
 #define _DT_BINDINGS_CLK_QCOM_TCSR_CC_ELIZA_H
 
 /* TCSR_CC clocks */
-#define TCSR_PCIE_0_CLKREF_EN				0
-#define TCSR_UFS_CLKREF_EN				1
-#define TCSR_USB2_CLKREF_EN				2
-#define TCSR_USB3_CLKREF_EN				3
+#define TCSR_HDMI_CLKREF_EN				0
+#define TCSR_PCIE_0_CLKREF_EN				1
+#define TCSR_PCIE_1_CLKREF_EN				2
+#define TCSR_UFS_CLKREF_EN				3
+#define TCSR_USB2_CLKREF_EN				4
+#define TCSR_USB3_CLKREF_EN				5
 
 #endif
