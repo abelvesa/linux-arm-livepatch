ed604a0647427a9ff2de375affed702388bd998b
Abel Vesa <abel.vesa@oss.qualcomm.com>
2026-02-18 10:19:38 +0200

    WIP

--- drivers/clk/qcom/gcc-eliza.c
+++ drivers/clk/qcom/gcc-eliza.c
@@ -2826,7 +2826,7 @@ static struct gdsc gcc_ufs_mem_phy_gdsc = {
 		.name = "gcc_ufs_mem_phy_gdsc",
 	},
 	.pwrsts = PWRSTS_OFF_ON,
-	.flags = POLL_CFG_GDSCR | RETAIN_FF_ENABLE,
+	.flags = POLL_CFG_GDSCR | RETAIN_FF_ENABLE | ALWAYS_ON,
 };
 
 static struct gdsc gcc_ufs_phy_gdsc = {
@@ -2838,7 +2838,7 @@ static struct gdsc gcc_ufs_phy_gdsc = {
 		.name = "gcc_ufs_phy_gdsc",
 	},
 	.pwrsts = PWRSTS_OFF_ON,
-	.flags = VOTABLE | POLL_CFG_GDSCR | RETAIN_FF_ENABLE,
+	.flags = VOTABLE | POLL_CFG_GDSCR | RETAIN_FF_ENABLE | ALWAYS_ON,
 };
 
 static struct gdsc gcc_usb30_prim_gdsc = {
